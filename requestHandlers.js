4bd542d01bf5c0b88d6433dcb3e1303b85f10dae
yancan1121 <871661582@qq.com>
2018-02-05 18:12:28 +0800

    tiansongtao

--- requestHandlers.js
+++ requestHandlers.js
@@ -29,9 +29,18 @@ function upload(response,postData) {
 }
 function show(response, postData) {
     console.log("Request Handler 'show' was called");
-    fs.readFile("","binary",function(){
-
+    fs.readFile("./tmp/test.png","binary",function(error,file){
+        if(error){
+            response.writeHead(500,{'Content-Type':'text/plain'});
+            response.write(error+'\n');
+            response.end();
+        }else{
+            response.writeHead(500,{'Content-Type':'image/png'});
+            response.write(file,'binary');
+            response.end();
+        }
     })
 }
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
